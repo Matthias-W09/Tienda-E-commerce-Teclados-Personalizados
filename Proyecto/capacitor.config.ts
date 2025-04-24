403b1b869c97622fb2d1413a222426ff7090aa4b
Matth14503w <155279145+Matthias-W09@users.noreply.github.com>
2025-04-23 23:17:09 -0400

    Add files via upload

---
+++ Proyecto/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.proyectorokubi',
+  appName: 'proyectorokubi',
+  webDir: 'www'
+};
+
+export default config;
