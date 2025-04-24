aa8c50f73c817f5c9261d76a946be609e49af8db
Matth14503w <155279145+Matthias-W09@users.noreply.github.com>
2025-04-23 23:50:41 -0400

    Delete Proyecto directory

--- Proyecto/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'ionic.proyectorokubi',
-  appName: 'proyectorokubi',
-  webDir: 'www'
-};
-
-export default config;
